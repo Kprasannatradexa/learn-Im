096c0ca5fd19ca7eb66eb17e28ef0c77f1e8b888
Kprasannatradexa <prasanna@tradexa.co.in>
2023-02-07 13:11:12 +0530

    Shared module imported to AuthenticationModule.

--- src/app/authentication/authentication.module.ts
+++ src/app/authentication/authentication.module.ts
@@ -5,6 +5,7 @@ import { AuthenticationRoutingModule } from './authentication-routing.module';
 import { SharedModule } from '../shared/shared.module';
 import { AuthenticationContainerComponent } from './components/authentication-container/authentication-container.component';
 import { LoginComponent } from './components/login/login.component';
+import { InputModule } from '../components/input/input/input.module';
 
 
 @NgModule({
@@ -15,7 +16,8 @@ import { LoginComponent } from './components/login/login.component';
   imports: [
     CommonModule,
     AuthenticationRoutingModule,
-    SharedModule
+    SharedModule,
+    InputModule
   ]
 })
 export class AuthenticationModule { }
