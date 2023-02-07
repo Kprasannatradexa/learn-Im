24913b17129e8447df699075c0b4f2bb6233175d
Kprasannatradexa <prasanna@tradexa.co.in>
2023-02-07 13:30:43 +0530

    Input component created.

--- src/app/components/input/input/input.component.ts
+++ src/app/components/input/input/input.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output } from '@angular/core';
+import { IMAGE_URLS } from 'src/app/core/constants/image-source';
+import { EventEmitter } from 'stream';
 
 @Component({
   selector: 'app-input',
   templateUrl: './input.component.html',
   styleUrls: ['./input.component.scss']
 })
-export class InputComponent implements OnInit {
+export class InputComponent {
 
-  constructor() { }
+  IMAGE_URLS = IMAGE_URLS
 
-  ngOnInit(): void {
+  @Input() type: string = '';
+  @Input() label: string = '';
+  @Input() placeholder: string = '';
+  @Input() control: any;
+  @Input() errorMessages: any;
+  @Input() width!: number;
+  @Input() errorType: string = '';
+  @Input() required: boolean = false;
+  @Input() isNewPassword: boolean = false;
+  @Input() isPasswordInput: boolean = false;
+  @Input() passwordInfo: boolean = false;
+
+
+  // @Output() keyDown = new EventEmitter();
+  // @Output() infoToggle$ = new EventEmitter();
+
+
+  public maskedPassword: boolean = false;
+
+  togglePasswordView() {
+    this.type = this.type === 'text' ? 'password' : 'text';
+    this.maskedPassword = !this.maskedPassword;
   }
+
+  get errorMessage() {
+    if (this.errorMessages?.[Object.keys(this?.control?.errors)?.[0]] !== undefined) {
+      return this.errorMessages[Object.keys(this.control.errors)[0]];
+    }
+    return '';
+  }
+
+
 
 }
