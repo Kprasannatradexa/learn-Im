b76d5cf8a750afeb9689365cb284e8f82cb4a612
Kprasannatradexa <prasanna@tradexa.co.in>
2023-02-07 11:35:15 +0530

    Login routing path added to routes.

--- src/app/authentication/authentication-routing.module.ts
+++ src/app/authentication/authentication-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthenticationContainerComponent } from './components/authentication-container/authentication-container.component';
+import { LoginComponent } from './components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  {
+    path: '', component: AuthenticationContainerComponent,
+    children: [
+      { path: 'login', component: LoginComponent }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
