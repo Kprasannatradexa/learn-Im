f52fba47dbc0527e641cb70f2d3116a13a8de825
Kprasannatradexa <prasanna@tradexa.co.in>
2023-02-07 13:10:51 +0530

    Shared module imported to inputModule and inputComponent exported from module.

--- src/app/components/input/input/input.module.ts
+++ src/app/components/input/input/input.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InputComponent } from './input.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 
 
@@ -9,7 +10,11 @@ import { InputComponent } from './input.component';
     InputComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
+  ],
+  exports: [
+    InputComponent
   ]
 })
 export class InputModule { }
