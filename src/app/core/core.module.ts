5dd8ebc7e2d55be3c407f90b4f1f48350d165049
Kprasannatradexa <prasanna@tradexa.co.in>
2023-02-09 17:13:05 +0530

    Shared module added.

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AuthInterceptor } from './services/interceptor/auth.interceptor';
 import { ServerInterceptor } from './services/interceptor/server.interceptor';
+import { SharedModule } from '../shared/shared.module';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
   ],
   providers: [
     {
