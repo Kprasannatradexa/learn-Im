c2a1362185fbeef6836c708062533d0a5e695e02
Kprasannatradexa <prasanna@tradexa.co.in>
2023-02-09 13:14:10 +0530

    UserSession stored in user$.

--- src/app/core/services/user/user.service.ts
+++ src/app/core/services/user/user.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { UserSession } from '../../models/user.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
+  private user = new BehaviorSubject<UserSession | null>(null);
+  user$ = this.user.asObservable();
+
   constructor() { }
+
+  set appUser(user) {
+    this.user.next(user)
+  }
+
+  get appUser() {
+    return this.user.value;
+  }
 }
