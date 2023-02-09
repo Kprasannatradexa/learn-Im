92afb372737583aab337749e65f56348310de877
Kprasannatradexa <prasanna@tradexa.co.in>
2023-02-09 17:21:34 +0530

    Spinner module imported.

--- src/app/components/button-lg/button-lg.module.ts
+++ src/app/components/button-lg/button-lg.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ButtonLgComponent } from './button-lg.component';
+import { SpinnerModule } from '../spinner/spinner.module';
 
 
 
 @NgModule({
   declarations: [
+    ButtonLgComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SpinnerModule
+  ],
+  exports: [
+    ButtonLgComponent
   ]
 })
 export class ButtonLgModule { }
