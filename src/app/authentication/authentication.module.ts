9954890d0eef0d19d642e9c8c83f6565b74816a0
Kprasannatradexa <prasanna@tradexa.co.in>
2023-02-09 10:44:30 +0530

    RegistrationComponent created.

--- src/app/authentication/authentication.module.ts
+++ src/app/authentication/authentication.module.ts
@@ -6,12 +6,14 @@ import { SharedModule } from '../shared/shared.module';
 import { AuthenticationContainerComponent } from './components/authentication-container/authentication-container.component';
 import { LoginComponent } from './components/login/login.component';
 import { InputModule } from '../components/input/input/input.module';
+import { RegistrationComponent } from './components/registration/registration.component';
 
 
 @NgModule({
   declarations: [
     AuthenticationContainerComponent,
-    LoginComponent
+    LoginComponent,
+    RegistrationComponent
   ],
   imports: [
     CommonModule,
