014527ab8d0e89328da28da196c4a79b38e8f875
Kprasannatradexa <prasanna@tradexa.co.in>
2023-02-09 17:11:41 +0530

    Register routing path added.

--- src/app/authentication/authentication-routing.module.ts
+++ src/app/authentication/authentication-routing.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthenticationContainerComponent } from './components/authentication-container/authentication-container.component';
 import { LoginComponent } from './components/login/login.component';
+import { RegistrationComponent } from './components/registration/registration.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   {
     path: '', component: AuthenticationContainerComponent,
     children: [
-      { path: 'login', component: LoginComponent }
+      { path: 'login', component: LoginComponent },
+      { path: 'register', component: RegistrationComponent }
     ]
   }
 ];
