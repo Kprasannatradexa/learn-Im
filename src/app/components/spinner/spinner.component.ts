c788c5bb65772f9ed378ce4d84831c1080a8c33b
Kprasannatradexa <prasanna@tradexa.co.in>
2023-02-09 15:52:10 +0530

    Button spinner added.

--- src/app/components/spinner/spinner.component.ts
+++ src/app/components/spinner/spinner.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-spinner',
   templateUrl: './spinner.component.html',
   styleUrls: ['./spinner.component.scss']
 })
-export class SpinnerComponent implements OnInit {
+export class SpinnerComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  @Input() diameter: string = '3';
 
 }
