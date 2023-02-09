2c73636711c73f3202bfdd8ca277996aa135b65a
Kprasannatradexa <prasanna@tradexa.co.in>
2023-02-09 13:24:21 +0530

    Interceptor provided to core-module.

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AuthInterceptor } from './services/interceptor/auth.interceptor';
+import { ServerInterceptor } from './services/interceptor/server.interceptor';
 
 
 
@@ -7,6 +10,18 @@ import { CommonModule } from '@angular/common';
   declarations: [],
   imports: [
     CommonModule
+  ],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: ServerInterceptor,
+      multi: true
+    }
   ]
 })
 export class CoreModule { }
