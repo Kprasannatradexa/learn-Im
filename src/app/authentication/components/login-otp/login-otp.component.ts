dc7d3305441d2ca13d95abdbd72caba389a327fc
Kprasannatradexa <prasanna@tradexa.co.in>
2023-02-10 11:57:48 +0530

    send otp response is handled.

--- src/app/authentication/components/login-otp/login-otp.component.ts
+++ src/app/authentication/components/login-otp/login-otp.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ReplaySubject, takeUntil } from 'rxjs';
 import { IMAGE_URLS } from 'src/app/core/constants/image-source';
 import { CustomValidators } from 'src/app/core/constants/validator';
 import { AuthenticationRepositoryService } from '../../services/authentication-repository.service';
@@ -11,10 +13,13 @@ import { AuthenticationRepositoryService } from '../../services/authentication-r
 })
 export class LoginOtpComponent {
 
+  private desytroyed$: ReplaySubject<boolean> = new ReplaySubject(1);
 
-
-  constructor(private fb: FormBuilder,
-    private authenticationRepositoryService: AuthenticationRepositoryService) { }
+  constructor(
+    private fb: FormBuilder,
+    private authenticationRepositoryService: AuthenticationRepositoryService,
+    private router: Router
+  ) { }
 
   login_credential = this.fb.control('', [Validators.required, CustomValidators.noWhiteSpaceValidator, CustomValidators.email]);
 
@@ -22,10 +27,23 @@ export class LoginOtpComponent {
   sendOtp() {
     this.login_credential.markAllAsTouched;
     if (this.login_credential.valid) {
+
+      const login_credential = this.login_credential.value;
+
       const requestObj = {
-        user_name: this.login_credential.value
+        user_name: login_credential
       }
-      this.authenticationRepositoryService.sendOtp(requestObj)
+      this.authenticationRepositoryService.sendOtp(requestObj).pipe(
+        takeUntil(this.desytroyed$)
+      ).subscribe({
+        next: ((success) => {
+          this.authenticationRepositoryService.currentLoginCredentials = login_credential;
+          this.router.navigate(['/auth/verify-otp'], { skipLocationChange: true, queryParamsHandling: 'preserve' })
+        }),
+        error: ((error) => {
+          console.log(error);
+        })
+      })
     }
   }
 
