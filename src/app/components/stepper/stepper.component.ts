de593b940b5009dc6702c8bf07cf7232c4794c6a
Kprasannatradexa <prasanna@tradexa.co.in>
2023-02-10 18:17:14 +0530

    Providers added.

--- src/app/components/stepper/stepper.component.ts
+++ src/app/components/stepper/stepper.component.ts
@@ -1,11 +1,12 @@
 import { CdkStepper } from '@angular/cdk/stepper';
-import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, Input, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { ReplaySubject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-stepper',
   templateUrl: './stepper.component.html',
-  styleUrls: ['./stepper.component.scss']
+  styleUrls: ['./stepper.component.scss'],
+  providers: [{ provide: CdkStepper, useExisting: StepperComponent }],
 })
 export class StepperComponent extends CdkStepper {
 
