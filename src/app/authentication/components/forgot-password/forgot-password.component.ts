73721689668979a0b3519beab741e2ca05d7a0fc
Kprasannatradexa <prasanna@tradexa.co.in>
2023-02-10 17:26:36 +0530

    login_credential , otp formControls and resetPasswordForm formGroup were created.

--- src/app/authentication/components/forgot-password/forgot-password.component.ts
+++ src/app/authentication/components/forgot-password/forgot-password.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { ReplaySubject, takeUntil } from 'rxjs';
 import { CustomValidators } from 'src/app/core/constants/validator';
+import { AuthenticationRepositoryService } from '../../services/authentication-repository.service';
 
 @Component({
   selector: 'app-forgot-password',
@@ -10,14 +12,21 @@ import { CustomValidators } from 'src/app/core/constants/validator';
 export class ForgotPasswordComponent {
 
   constructor(
-    private fb: FormBuilder
+    private fb: FormBuilder,
+    private authenticationRepositoryService: AuthenticationRepositoryService
   ) { }
+
+  private destroyed$: ReplaySubject<boolean> = new ReplaySubject(1);
+
 
   login_credential = this.fb.control('', [Validators.required, CustomValidators.noWhiteSpaceValidator, CustomValidators.email])
 
 
-  sendOtp() {
+  otp = this.fb.control(null, { updateOn: 'blur', validators: CustomValidators.otp })
 
-  }
+  resetPasswordForm = this.fb.group({
+    password: ['', [CustomValidators.required, CustomValidators.password]],
+    new_password: ['', [CustomValidators.required, CustomValidators.password]]
+  })
 
 }
