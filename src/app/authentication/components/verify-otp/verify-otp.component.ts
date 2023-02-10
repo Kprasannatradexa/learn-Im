0ab05961820123f4b7af9efa591898d3c6696573
Kprasannatradexa <prasanna@tradexa.co.in>
2023-02-10 14:17:56 +0530

    verifyOtp and resendOtp methods were added.

--- src/app/authentication/components/verify-otp/verify-otp.component.ts
+++ src/app/authentication/components/verify-otp/verify-otp.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ReplaySubject, takeUntil } from 'rxjs';
 import { CustomValidators } from 'src/app/core/constants/validator';
 import { AuthenticationRepositoryService } from '../../services/authentication-repository.service';
 
@@ -12,6 +14,8 @@ export class VerifyOtpComponent {
 
   buttonLoading: boolean = false;
 
+  private destroyed$: ReplaySubject<boolean> = new ReplaySubject(1);
+
   OTP_CONFIG = {
     length: 6,
     allowNumbersOnly: true,
@@ -22,6 +26,7 @@ export class VerifyOtpComponent {
   constructor(
     private fb: FormBuilder,
     private authenticationRepositoryService: AuthenticationRepositoryService,
+    private router: Router
   ) { }
 
   otp = this.fb.control(null, { updateOn: 'blur', validators: CustomValidators.otp })
@@ -32,11 +37,46 @@ export class VerifyOtpComponent {
   }
 
   verifyOtp() {
+    this.otp.markAllAsTouched;
+
+    if (this.otp.valid) {
+      this.authenticationRepositoryService.verifyOtp({ otp: this.otp.value }).pipe(
+        takeUntil(this.destroyed$)
+      ).subscribe({
+        next: ((response) => {
+          if (response?.access_token) {
+            console.log('Your logged in');
+            this.router.navigate(['/login'])
+          }
+        }),
+        error: ((error) => {
+          console.log(error);
+        })
+      })
+    }
 
   }
 
 
   resendOTP() {
+    const currentLoginCredentials = this.authenticationRepositoryService.currentLoginCredentials;
+
+    if (!currentLoginCredentials) {
+      console.log("Cannot resend OTP");
+      this.router.navigate(['/auth/login-otp'])
+    }
+
+    this.authenticationRepositoryService.sendOtp({ username: currentLoginCredentials }).pipe(
+      takeUntil(this.destroyed$)
+    ).subscribe({
+      next: ((success) => {
+        console.log('OTP sent');
+        this.router.navigate(['/auth/verify-otp'], { skipLocationChange: true, queryParamsHandling: 'preserve' })
+      }),
+      error: ((error) => {
+        console.log(error);
+      })
+    })
 
   }
 }
