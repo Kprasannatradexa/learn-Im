3baa3135df4f7335216cb474639b4bdb34bd48ce
Kprasannatradexa <prasanna@tradexa.co.in>
2023-02-10 16:49:18 +0530

    Stepper service method added to move to next or previous step.

--- src/app/components/stepper/stepper.service.ts
+++ src/app/components/stepper/stepper.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StepperService {
 
-  constructor() { }
+  private previousStep = new Subject();
+  previousStep$ = this.previousStep.asObservable();
+
+  private currentStep = new BehaviorSubject(0);
+  currentStep$ = this.currentStep.asObservable();
+
+  private nextStep = new Subject();
+  nextStep$ = this.nextStep.asObservable();
+
+  next() {
+    this.nextStep.next(true);
+  }
+
+  previous() {
+    this.previousStep.next(true);
+  }
+
+  set step(step: number) {
+    this.currentStep.next(step)
+  }
+
+  get step() {
+    return this.currentStep.value;
+  }
 }
