a2e601649c57fe5ce944c11358abf1300b5b17d6
Kprasannatradexa <prasanna@tradexa.co.in>
2023-02-10 10:36:49 +0530

    login-otp added to Routes object.

--- src/app/authentication/authentication-routing.module.ts
+++ src/app/authentication/authentication-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthenticationContainerComponent } from './components/authentication-container/authentication-container.component';
+import { LoginOtpComponent } from './components/login-otp/login-otp.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegistrationComponent } from './components/registration/registration.component';
 
@@ -10,7 +11,8 @@ const routes: Routes = [
     path: '', component: AuthenticationContainerComponent,
     children: [
       { path: 'login', component: LoginComponent },
-      { path: 'register', component: RegistrationComponent }
+      { path: 'register', component: RegistrationComponent },
+      { path: 'login-otp', component: LoginOtpComponent }
     ]
   }
 ];
