a1ae220fa100ef38944078de09b1862717ceed6d
Kprasannatradexa <prasanna@tradexa.co.in>
2023-02-10 17:01:22 +0530

    StepperComponent exported from module.

--- src/app/components/stepper/stepper.module.ts
+++ src/app/components/stepper/stepper.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StepperComponent } from './stepper.component';
-
+import { CdkStepperModule } from '@angular/cdk/stepper';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 
 @NgModule({
@@ -9,7 +10,12 @@ import { StepperComponent } from './stepper.component';
     StepperComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    CdkStepperModule,
+    SharedModule
+  ],
+  exports: [
+    StepperComponent
   ]
 })
 export class StepperModule { }
