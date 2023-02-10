a884b7ecc559a2700787007fa457a1c992305970
Kprasannatradexa <prasanna@tradexa.co.in>
2023-02-10 12:40:54 +0530

    otp formControl created and OTP_CONFIG object created.

--- src/app/authentication/components/verify-otp/verify-otp.component.ts
+++ src/app/authentication/components/verify-otp/verify-otp.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { CustomValidators } from 'src/app/core/constants/validator';
+import { AuthenticationRepositoryService } from '../../services/authentication-repository.service';
 
 @Component({
   selector: 'app-verify-otp',
   templateUrl: './verify-otp.component.html',
   styleUrls: ['./verify-otp.component.scss']
 })
-export class VerifyOtpComponent implements OnInit {
+export class VerifyOtpComponent {
 
-  constructor() { }
+  buttonLoading: boolean = false;
 
-  ngOnInit(): void {
+  OTP_CONFIG = {
+    length: 6,
+    allowNumbersOnly: true,
+    containerClass: 'otp-container',
+    inputClass: 'otp-input'
   }
 
+  constructor(
+    private fb: FormBuilder,
+    private authenticationRepositoryService: AuthenticationRepositoryService,
+  ) { }
+
+  otp = this.fb.control(null, { updateOn: 'blur', validators: CustomValidators.otp })
+
+
+  onOtpChange(value: string) {
+    this.otp.setValue(value)
+  }
+
+  verifyOtp() {
+
+  }
+
+
+  resendOTP() {
+
+  }
 }
