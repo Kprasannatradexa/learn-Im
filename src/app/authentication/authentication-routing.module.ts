42eb968fbfad1b1b4d4360441ea174f8dc16f311
Kprasannatradexa <prasanna@tradexa.co.in>
2023-02-10 12:26:32 +0530

    Formatted route object.

--- src/app/authentication/authentication-routing.module.ts
+++ src/app/authentication/authentication-routing.module.ts
@@ -4,15 +4,32 @@ import { AuthenticationContainerComponent } from './components/authentication-co
 import { LoginOtpComponent } from './components/login-otp/login-otp.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegistrationComponent } from './components/registration/registration.component';
+import { VerifyOtpComponent } from './components/verify-otp/verify-otp.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   {
     path: '', component: AuthenticationContainerComponent,
     children: [
-      { path: 'login', component: LoginComponent },
-      { path: 'register', component: RegistrationComponent },
-      { path: 'login-otp', component: LoginOtpComponent }
+      {
+        path: 'login',
+        component: LoginComponent
+      },
+      {
+        path: 'register',
+        component: RegistrationComponent
+      },
+      {
+        path: 'login-otp',
+        component: LoginOtpComponent
+      },
+      {
+        path: 'verify-otp',
+        component: VerifyOtpComponent,
+        data: {
+          redirectRoute: '/auth/login-otp'
+        },
+      }
     ]
   }
 ];
