a0fb3ee87722fdf7fe168a9b897e15c773c4d81d
Kprasannatradexa <prasanna@tradexa.co.in>
2023-02-10 11:40:43 +0530

    sendOtp() method added.

--- src/app/authentication/components/login-otp/login-otp.component.ts
+++ src/app/authentication/components/login-otp/login-otp.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { IMAGE_URLS } from 'src/app/core/constants/image-source';
 import { CustomValidators } from 'src/app/core/constants/validator';
+import { AuthenticationRepositoryService } from '../../services/authentication-repository.service';
 
 @Component({
   selector: 'app-login-otp',
@@ -9,13 +11,22 @@ import { CustomValidators } from 'src/app/core/constants/validator';
 })
 export class LoginOtpComponent {
 
-  constructor(private fb: FormBuilder) { }
+
+
+  constructor(private fb: FormBuilder,
+    private authenticationRepositoryService: AuthenticationRepositoryService) { }
 
   login_credential = this.fb.control('', [Validators.required, CustomValidators.noWhiteSpaceValidator, CustomValidators.email]);
 
 
   sendOtp() {
-
+    this.login_credential.markAllAsTouched;
+    if (this.login_credential.valid) {
+      const requestObj = {
+        user_name: this.login_credential.value
+      }
+      this.authenticationRepositoryService.sendOtp(requestObj)
+    }
   }
 
 }
