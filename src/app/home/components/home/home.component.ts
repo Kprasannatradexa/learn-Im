75909fb04f54fd9cf14a48aedec249648f34f5ae
Kprasannatradexa <prasanna@tradexa.co.in>
2023-02-13 17:27:01 +0530

    Search added with searchCourses methods.

--- src/app/home/components/home/home.component.ts
+++ src/app/home/components/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { debounceTime, distinctUntilChanged, ReplaySubject, Subject } from 'rxjs';
+import { HomeApiService } from '../../services/home-api.service';
 
 @Component({
   selector: 'app-home',
@@ -7,15 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  searchQuery = new Subject();
+  searchQuery$ = this.searchQuery.asObservable();
+
+  destroyed$: ReplaySubject<boolean> = new ReplaySubject(1);
+
+  course: any;
+
+  constructor(private homeApiService: HomeApiService) { }
 
   ngOnInit(): void {
+
+    this.searchQuery$.subscribe((searchValue: any) => {
+      console.log(searchValue);
+      this.getSearchedCourses(searchValue);
+    })
+
   }
 
 
+  getSearchedCourses(searchValue: string) {
+    this.course = this.homeApiService.getSearchedCourse(searchValue).subscribe();
+  }
 
   searchCourses(value: string) {
-
+    if (value !== '') {
+      this.searchQuery.next(value);
+    }
   }
 
 }
