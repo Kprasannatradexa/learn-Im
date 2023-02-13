0e1beec1753eb4b1043a499d32a8be5d5f26a573
Kprasannatradexa <prasanna@tradexa.co.in>
2023-02-13 11:30:11 +0530

    Shared module imported.

--- src/app/booking/booking.module.ts
+++ src/app/booking/booking.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { BookingRoutingModule } from './booking-routing.module';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    BookingRoutingModule
+    BookingRoutingModule,
+    SharedModule
   ]
 })
 export class BookingModule { }
