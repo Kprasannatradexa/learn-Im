93a668b6faabc3628117202663954864956c7024
Kprasannatradexa <prasanna@tradexa.co.in>
2023-02-13 17:29:04 +0530

    get Courses api integrated.

--- src/app/home/services/home-api.service.ts
+++ src/app/home/services/home-api.service.ts
@@ -1,9 +1,27 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { EnvironmentService } from 'src/app/core/services/environment/environment.service';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HomeApiService {
 
-  constructor() { }
+  url: string = environment.baseUrl;
+
+  constructor(private http: HttpClient,
+    private environmentService: EnvironmentService) {
+    // this.environmentService.environment$.subscribe((url) => {
+    //   if (url) {
+    //     this.url = url;
+    //   }
+    // })
+  }
+
+  getCourses() {
+    return this.http.get(`${this.url}/courses/`)
+  }
+
+
 }
