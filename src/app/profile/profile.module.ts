821f254addeb14063c452992fc240b3cf9898bf5
Kprasannatradexa <prasanna@tradexa.co.in>
2023-02-14 16:16:49 +0530

    ProfileLayoutComponent created.

--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ProfileRoutingModule } from './profile-routing.module';
+import { ProfileLayoutComponent } from './components/profile-layout/profile-layout.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ProfileLayoutComponent
+  ],
   imports: [
     CommonModule,
     ProfileRoutingModule
