60db4024820bab759be7ba88e84a3931de86c03e
Kprasannatradexa <prasanna@tradexa.co.in>
2023-02-14 19:02:13 +0530

    Book slot component configured.

--- src/app/booking/components/select-time-slot/select-time-slot.component.ts
+++ src/app/booking/components/select-time-slot/select-time-slot.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-select-time-slot',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SelectTimeSlotComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('date') date!: ElementRef;
+  @ViewChild('selectedOption') selectedOption!: ElementRef;
+
+  constructor(private fb: FormBuilder) { }
+
+  selected_date = this.fb.control([new Date(), [Validators.required]])
 
   ngOnInit(): void {
   }
+
+  redirectTo() {
+
+  }
+
+  bookASlot() {
+    const selectedDate = this.date.nativeElement.value;
+    const selectedTimeSlot = this.selectedOption.nativeElement.value;
+
+    console.log(selectedDate, selectedTimeSlot);
+
+  }
+
+  options = [
+    { start_time: "09:00:00", end_time: "12:00:00" },
+    { start_time: "01:00:00", end_time: "04:00:00" },
+    { start_time: "05:00:00", end_time: "08:00:00" },
+  ]
+
 
 }
