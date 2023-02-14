7fc5aa132251999c0bc0a5cda31ffacd08b40e26
Kprasannatradexa <prasanna@tradexa.co.in>
2023-02-14 11:03:45 +0530

    InvoiceUploadComponent created.

--- src/app/booking/booking.module.ts
+++ src/app/booking/booking.module.ts
@@ -3,10 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { BookingRoutingModule } from './booking-routing.module';
 import { SharedModule } from '../shared/shared.module';
+import { InvoiceUploadComponent } from './components/invoice-upload/invoice-upload.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    InvoiceUploadComponent
+  ],
   imports: [
     CommonModule,
     BookingRoutingModule,
