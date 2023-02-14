20abb6a0e21b7f12db3f55a6cdac95820c04cf6a
Kprasannatradexa <prasanna@tradexa.co.in>
2023-02-14 16:58:37 +0530

    Routing path were added.

--- src/app/profile/profile-routing.module.ts
+++ src/app/profile/profile-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CompletedCoursesComponent } from './components/completed-courses/completed-courses.component';
+import { MyCoursesComponent } from './components/my-courses/my-courses.component';
+import { MyProfileComponent } from './components/my-profile/my-profile.component';
+import { ProfileLayoutComponent } from './components/profile-layout/profile-layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: ProfileLayoutComponent, children: [
+      { path: '', component: MyCoursesComponent },
+      { path: 'completed-courses', component: CompletedCoursesComponent },
+      { path: 'information', component: MyProfileComponent },
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
