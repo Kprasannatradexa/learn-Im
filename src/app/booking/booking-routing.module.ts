17ffbdc93d9df25bdfe55bf41173508552198e37
Kprasannatradexa <prasanna@tradexa.co.in>
2023-02-14 19:01:35 +0530

    SelectTimeSlotComponent added to route.

--- src/app/booking/booking-routing.module.ts
+++ src/app/booking/booking-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InvoiceUploadComponent } from './components/invoice-upload/invoice-upload.component';
+import { SelectTimeSlotComponent } from './components/select-time-slot/select-time-slot.component';
 
 const routes: Routes = [
-  { path: '', component: InvoiceUploadComponent }
+  { path: '', component: SelectTimeSlotComponent },
+  { path: 'upload', component: InvoiceUploadComponent }
 ];
 
 @NgModule({
