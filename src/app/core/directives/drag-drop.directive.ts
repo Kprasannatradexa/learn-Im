92818b48a94d2786801c5edd4b80a05aa61ba447
Kprasannatradexa <prasanna@tradexa.co.in>
2023-02-14 11:30:24 +0530

    appDragDrop directive updated with dragover, dragleave and drop host listeners.

--- src/app/core/directives/drag-drop.directive.ts
+++ src/app/core/directives/drag-drop.directive.ts
@@ -1,4 +1,4 @@
-import { Directive } from '@angular/core';
+import { Directive, EventEmitter, HostListener, Output } from '@angular/core';
 
 @Directive({
   selector: '[appDragDrop]'
@@ -7,4 +7,27 @@ export class DragDropDirective {
 
   constructor() { }
 
+  @Output() fileDropped = new EventEmitter();
+
+  @HostListener('dragover', ['$event']) onDragOver(event: DragEvent) {
+
+    event.stopPropagation();
+    event.preventDefault();
+  }
+
+  @HostListener('dragleave', ['$event']) onDragLeave(event: DragEvent) {
+    event.stopPropagation();
+    event.preventDefault();
+  }
+
+  @HostListener('drop', ['$event']) onDrop(event: any) {
+    event.stopPropagation();
+    event.preventDefault();
+
+    const file = event.dataTransfer.files;
+
+    if (file.length > 0) {
+      this.fileDropped.emit(file);
+    }
+  }
 }
