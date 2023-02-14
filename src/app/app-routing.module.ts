79303baaa35d7eb73a8056b6cfb4febb6bc8d3b0
Kprasannatradexa <prasanna@tradexa.co.in>
2023-02-14 11:30:52 +0530

    Booking routing path added to root route.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeModule } from './home/home.module';
+import { AuthenticationGuard } from './shared/guards/authentication.guard';
 
 const routes: Routes = [
   { path: '', loadChildren: () => HomeModule },
-  { path: 'auth', loadChildren: () => import('./authentication/authentication.module').then(m => m.AuthenticationModule) }
+  { path: 'auth', loadChildren: () => import('./authentication/authentication.module').then(m => m.AuthenticationModule) },
+  { path: 'booking', canActivate: [AuthenticationGuard], loadChildren: () => import('./booking/booking.module').then(m => m.BookingModule) }
 ];
 
 @NgModule({
