420190a352718442e12cfa5be3d0534378d90ea0
Kprasannatradexa <prasanna@tradexa.co.in>
2023-02-14 10:09:02 +0530

    getSearchedCourses and getCourseLocation methods were added.

--- src/app/home/components/home/home.component.ts
+++ src/app/home/components/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { debounceTime, distinctUntilChanged, ReplaySubject, Subject } from 'rxjs';
+import { debounceTime, distinctUntilChanged, Observable, ReplaySubject, Subject } from 'rxjs';
 import { HomeApiService } from '../../services/home-api.service';
 
 @Component({
@@ -9,21 +9,51 @@ import { HomeApiService } from '../../services/home-api.service';
 })
 export class HomeComponent implements OnInit {
 
-  searchQuery = new Subject<string>();
-  searchQuery$ = this.searchQuery.asObservable();
+  searchCourse = new Subject<string>();
+  searchCourse$ = this.searchCourse.asObservable();
+
+  searchLocation = new Subject<string>();
+  searchLocation$ = this.searchLocation.asObservable();
 
   destroyed$: ReplaySubject<boolean> = new ReplaySubject(1);
 
+  allCourses: any;
+
   course: any;
+
+  location: any;
+
+  url: any
 
   constructor(private homeApiService: HomeApiService) { }
 
   ngOnInit(): void {
 
-    this.searchQuery$.subscribe((searchValue: string) => {
+    this.homeApiService.getCourses().subscribe((response) => {
+      this.allCourses = response
+      console.log(response);
+
+    })
+
+    this.homeApiService.getInstitutes().subscribe((response) => {
+      console.log(response);
+
+    })
+
+    this.searchCourse$.pipe(
+      debounceTime(500)
+    ).subscribe((searchValue: string) => {
       console.log(searchValue);
       this.getSearchedCourses(searchValue);
     })
+
+    this.searchLocation$.pipe(
+      debounceTime(500)
+    ).
+      subscribe((searchValue: string) => {
+        console.log(searchValue);
+        this.getCourseLocation(searchValue);
+      })
 
   }
 
@@ -32,9 +62,21 @@ export class HomeComponent implements OnInit {
     this.course = this.homeApiService.getSearchedCourse(searchValue).subscribe();
   }
 
+  getCourseLocation(searchValue: string) {
+    this.location = this.homeApiService.getCoursesLocation(searchValue).subscribe((city) => {
+      console.log(city);
+    })
+  }
+
   searchCourses(value: string) {
     if (value !== '') {
-      this.searchQuery.next(value);
+      this.searchCourse.next(value);
+    }
+  }
+
+  searchCity(value: string) {
+    if (value !== '') {
+      this.searchLocation.next(value);
     }
   }
 
