06445bd567e4b75a807307ebd80c057faa7c9b7e
Kprasannatradexa <prasanna@tradexa.co.in>
2023-02-14 13:22:05 +0530

    uploadedFile object patched with selected file.

--- src/app/booking/components/invoice-upload/invoice-upload.component.ts
+++ src/app/booking/components/invoice-upload/invoice-upload.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-invoice-upload',
   templateUrl: './invoice-upload.component.html',
   styleUrls: ['./invoice-upload.component.scss']
 })
-export class InvoiceUploadComponent implements OnInit {
+export class InvoiceUploadComponent {
+
+
+  uploadedFile!: File | null;
 
   constructor() { }
 
-  ngOnInit(): void {
+
+  fileUploaded(file: FileList) {
+    this.uploadedFile = file[0];
   }
 
+  getSelectedFile(event: any) {
+    this.uploadedFile = event?.target?.files[0];
+  }
+
+  reset() {
+    this.uploadedFile = null
+  }
+
+  uploadInvoice() {
+    console.log(this.uploadedFile);
+
+  }
 }
