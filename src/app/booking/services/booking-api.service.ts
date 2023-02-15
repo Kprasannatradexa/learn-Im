042dba59aa75761a92d5c1f56ca5f9b1dc6947a6
Kprasannatradexa <prasanna@tradexa.co.in>
2023-02-15 10:14:52 +0530

    getCourseTimeSlots api service created.

--- src/app/booking/services/booking-api.service.ts
+++ src/app/booking/services/booking-api.service.ts
@@ -1,9 +1,25 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookingApiService {
 
-  constructor() { }
+
+  url: string = environment.baseUrl;
+
+  constructor(private http: HttpClient) { }
+
+  getCourseTimeslots(reqObject: any) {
+    const { id = '', date = '' } = reqObject || {};
+
+    return this.http.get(`${this.url}/courses/${id}/time_slots`, {
+      params: new HttpParams()
+        .set('slot_date', date)
+    })
+  }
+
+
 }
