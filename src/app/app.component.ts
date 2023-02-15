fdec82037cbfd80824de4b9f41594bbd1a38ec11
Kprasannatradexa <prasanna@tradexa.co.in>
2023-02-15 12:16:07 +0530

    autoLogin implemented in onint in root component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthenticationRepositoryService } from './authentication/services/authentication-repository.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'learn-im';
+
+  constructor(private authenticationRepositoryService: AuthenticationRepositoryService) { }
+
+  ngOnInit(): void {
+    this.authenticationRepositoryService.autoLogin();
+  }
 }
