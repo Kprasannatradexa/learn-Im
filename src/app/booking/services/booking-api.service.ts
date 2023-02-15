dbd1ff623aa4c6460b59cae1950d31137308d1e6
Kprasannatradexa <prasanna@tradexa.co.in>
2023-02-15 15:13:22 +0530

    Type added to get response of timeSlots.

--- src/app/booking/services/booking-api.service.ts
+++ src/app/booking/services/booking-api.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { BookingSlots } from '../interface/booking';
 
 @Injectable({
   providedIn: 'root'
@@ -12,10 +14,10 @@ export class BookingApiService {
 
   constructor(private http: HttpClient) { }
 
-  getCourseTimeslots(reqObject: any) {
+  getCourseTimeslots(reqObject: any): Observable<BookingSlots[]> {
     const { id = '', date = '' } = reqObject || {};
 
-    return this.http.get(`${this.url}/courses/${id}/time_slots`, {
+    return this.http.get<BookingSlots[]>(`${this.url}/courses/${id}/time_slots`, {
       params: new HttpParams()
         .set('slot_date', date)
     })
