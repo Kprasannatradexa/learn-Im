c970737f67fb3a6f001a667298c7163c629c8b07
Kprasannatradexa <prasanna@tradexa.co.in>
2023-02-15 18:07:53 +0530

    showSuccess, showInfo, showWarning, showError notification methods were created.

--- src/app/core/services/notification/notification.service.ts
+++ src/app/core/services/notification/notification.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { NotificationsService } from 'angular2-notifications';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NotificationService {
 
-  constructor() { }
+  constructor(private notificationsService: NotificationsService) { }
+
+  showSuccess(message: string) {
+    this.notificationsService.success(message, null, { theClass: 'success-notification' });
+  }
+
+  showInfo(message: string) {
+    this.notificationsService.info(message);
+  }
+
+  showWarning(message: string) {
+    this.notificationsService.warn(message);
+  }
+
+  showError(message: string) {
+    this.notificationsService.error(message);
+  }
+
+
 }
