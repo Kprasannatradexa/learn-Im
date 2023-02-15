e16f00f9199c8fd598bf4812e33a14d6862df203
Kprasannatradexa <prasanna@tradexa.co.in>
2023-02-15 17:52:25 +0530

    SimpleNotificationsModule imported to root module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { SimpleNotificationsModule } from 'angular2-notifications';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,6 +13,7 @@ import { CoreModule } from './core/core.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    SimpleNotificationsModule.forRoot(),
     CoreModule
   ],
   providers: [],
