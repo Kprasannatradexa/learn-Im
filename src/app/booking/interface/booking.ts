57aa1b9ab00fe9932b3a36eee5ecd549eaca8923
Kprasannatradexa <prasanna@tradexa.co.in>
2023-02-15 18:22:38 +0530

    BookedCourses created.

--- src/app/booking/interface/booking.ts
+++ src/app/booking/interface/booking.ts
@@ -1,3 +1,5 @@
+import { User } from "src/app/core/models/user.model";
+
 export interface BookingTimeSlots {
     id: string;
     slot_date: string;
@@ -7,4 +9,31 @@ export interface BookingTimeSlots {
 
 export interface TimeSlot {
     time_slot: string;
+}
+
+export interface BookedCourses {
+    id: string;
+    buyer: User;
+    course: CourseDetails;
+    time_slot: BookingTimeSlots;
+    booked_on: string;
+    booking_status: string;
+    payment_status: string;
+    pdf: any
+}
+
+export interface CourseDetails {
+    id: string;
+    title: string;
+    description: string;
+    price: string;
+    product: string[];
+    institute: string;
+    start_date: string;
+    end_date: string;
+    maximum_bookings: number;
+    time_slots: string[];
+    course_mode: string;
+    terms_and_conditions: string;
+    is_active: boolean;
 }
