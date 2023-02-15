bc8b42570f80aa601abc9d497ec011d36f110b7f
Kprasannatradexa <prasanna@tradexa.co.in>
2023-02-15 15:18:40 +0530

    getCourseTimeSlots and bookACourse api methods added.

--- src/app/booking/services/booking-repository.service.ts
+++ src/app/booking/services/booking-repository.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { BookingSlots } from '../interface/booking';
+import { BookingApiService } from './booking-api.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookingRepositoryService {
 
-  constructor() { }
+  constructor(private bookingApiService: BookingApiService) { }
+
+
+  getCourseTimeSlots(reqObject: any): Observable<BookingSlots[]> {
+    return this.bookingApiService.getCourseTimeSlots(reqObject);
+  }
+
+  bookACourse(reqObject: any) {
+    return this.bookingApiService.bookACourse(reqObject);
+  }
+
 }
