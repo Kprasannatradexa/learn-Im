68098f67cd09c56da6a08ca944da3ff6575eb168
Kprasannatradexa <prasanna@tradexa.co.in>
2023-02-15 12:16:45 +0530

    Json.parse added while retrieving data from local storage.

--- src/app/shared/guards/authentication.guard.ts
+++ src/app/shared/guards/authentication.guard.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { UserSession } from 'src/app/core/models/user.model';
 
 @Injectable({
   providedIn: 'root'
@@ -13,7 +14,7 @@ export class AuthenticationGuard implements CanActivate {
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
 
-    const isAuthenticated = localStorage.getItem('userData');
+    const isAuthenticated: UserSession = JSON.parse(localStorage.getItem('userData') ?? '');
 
     if (isAuthenticated) {
       return true;
