be3e0f653e92c755693de544f322de47ec9ecfed
Kprasannatradexa <prasanna@tradexa.co.in>
2023-02-16 17:00:15 +0530

    getter and setter added to courses.

--- src/app/home/components/available-courses/available-courses.component.ts
+++ src/app/home/components/available-courses/available-courses.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { CourseDetails } from 'src/app/booking/interface/booking';
 
 @Component({
   selector: 'app-available-courses',
   templateUrl: './available-courses.component.html',
   styleUrls: ['./available-courses.component.scss']
 })
-export class AvailableCoursesComponent implements OnInit {
+export class AvailableCoursesComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  @Input() set courses(courses: CourseDetails[]) {
+    this._courses = courses;
   }
+
+  get courses() {
+    return this._courses;
+  }
+
+  private _courses: CourseDetails[] = [];
 
 }
