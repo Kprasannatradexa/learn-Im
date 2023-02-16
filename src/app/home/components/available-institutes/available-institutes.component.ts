1bed339984b628e80b96ffa982ecac55a070790c
Kprasannatradexa <prasanna@tradexa.co.in>
2023-02-16 15:30:01 +0530

    getter and setter added to institutes.

--- src/app/home/components/available-institutes/available-institutes.component.ts
+++ src/app/home/components/available-institutes/available-institutes.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Institute } from '../../services/home-api.service';
 
 @Component({
   selector: 'app-available-institutes',
   templateUrl: './available-institutes.component.html',
   styleUrls: ['./available-institutes.component.scss']
 })
-export class AvailableInstitutesComponent implements OnInit {
+export class AvailableInstitutesComponent {
 
-  constructor() { }
+  @Input() set institutes(institutes: Institute[]) {
+    if (institutes.length >= 5) {
+      this._institutes = institutes.filter((institute, index) => {
+        if (index < 4) {
+          return institute;
+        }
+        return
+      })
+    } else {
+      this._institutes = institutes
+    }
 
-  ngOnInit(): void {
   }
+
+  get institutes() {
+    return this._institutes;
+  }
+
+  private _institutes: Institute[] = [];
 
 }
