a8e24be20146f65f13a78c26417c2c06a4b6991c
Kprasannatradexa <prasanna@tradexa.co.in>
2023-02-17 18:30:44 +0530

    getCourses method added to make getCourse call.

--- src/app/courses/services/course-repository.service.ts
+++ src/app/courses/services/course-repository.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CourseDetails } from 'src/app/booking/interface/booking';
+import { CourseApiService } from './course-api.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CourseRepositoryService {
 
-  constructor() { }
+  constructor(private courseApiService: CourseApiService) { }
+
+  getCourses(): Observable<CourseDetails[]> {
+    return this.courseApiService.getCourses();
+  }
 }
