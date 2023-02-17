ac20c0d65c0890dc9868119ae4e6c4a926eb1669
Kprasannatradexa <prasanna@tradexa.co.in>
2023-02-17 18:31:34 +0530

    courses$  created to store courses.

--- src/app/courses/components/courses/courses.component.ts
+++ src/app/courses/components/courses/courses.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { catchError, Observable, of } from 'rxjs';
+import { CourseDetails } from 'src/app/booking/interface/booking';
+import { NotificationService } from 'src/app/core/services/notification/notification.service';
+import { CourseRepositoryService } from '../../services/course-repository.service';
 
 @Component({
   selector: 'app-courses',
@@ -7,9 +11,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CoursesComponent implements OnInit {
 
-  constructor() { }
+  courses$!: Observable<CourseDetails[]>
+
+
+  constructor(private courseRepositoryService: CourseRepositoryService,
+    private notificationService: NotificationService) { }
 
   ngOnInit(): void {
+    this.courses$ = this.courseRepositoryService.getCourses().pipe(
+      catchError(() => {
+        this.notificationService.showWarning('Failed load instituted')
+        return of()
+      }))
   }
 
 }
