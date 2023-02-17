ea028d0ffd158ce48cdf216c040845a820d5582d
Kprasannatradexa <prasanna@tradexa.co.in>
2023-02-17 12:01:40 +0530

    MegaMenuModule created with MegaMenuComponent.

---
+++ src/app/components/mega-menu/mega-menu.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MegaMenuComponent } from './mega-menu.component';
+
+
+
+@NgModule({
+  declarations: [
+    MegaMenuComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    MegaMenuComponent
+  ]
+})
+export class MegaMenuModule { }
