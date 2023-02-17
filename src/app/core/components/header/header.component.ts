4468a868e29af93c686b3eadf2c9d407ad6a5a5f
Kprasannatradexa <prasanna@tradexa.co.in>
2023-02-17 15:58:00 +0530

    Header made fixed when scroll position exceed 66%.

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostListener, Input, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
+
+  @ViewChild('header') header!: ElementRef;
+
+  isScrolled: boolean = false
+
+  @HostListener('window:scroll', ['$event']) // for window scroll events
+
+  onScroll() {
+    const currentScrollPosition = window.pageYOffset;
+    const offsetHeight = this.header.nativeElement.offsetHeight;
+    if (currentScrollPosition > offsetHeight / 1.5) {
+      this.isScrolled = true;
+    } else {
+      this.isScrolled = false;
+    }
+  }
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
 
 }
