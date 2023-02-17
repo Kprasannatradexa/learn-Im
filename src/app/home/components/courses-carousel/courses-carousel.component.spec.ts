41ff94c303fefba3bb405f061aff7d90a0c39c43
Kprasannatradexa <prasanna@tradexa.co.in>
2023-02-17 11:04:46 +0530

    CoursesCarouselComponent created.

---
+++ src/app/home/components/courses-carousel/courses-carousel.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CoursesCarouselComponent } from './courses-carousel.component';
+
+describe('CoursesCarouselComponent', () => {
+  let component: CoursesCarouselComponent;
+  let fixture: ComponentFixture<CoursesCarouselComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CoursesCarouselComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CoursesCarouselComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
