59e5c0fee316a34ed305a33738428e316336f79b
Kprasannatradexa <prasanna@tradexa.co.in>
2023-02-17 11:43:59 +0530

    getCourses method added.

--- src/app/home/services/home-repository.service.ts
+++ src/app/home/services/home-repository.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CourseDetails } from 'src/app/booking/interface/booking';
+import { HomeApiService } from './home-api.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HomeRepositoryService {
 
-  constructor() { }
+  constructor(private homeApiService: HomeApiService) { }
+
+  getCourses(): Observable<CourseDetails[]> {
+    return this.homeApiService.getCourses();
+  }
 }
