e3dc11683f624e29109c9ce10d064f98e9941322
Kprasannatradexa <prasanna@tradexa.co.in>
2023-02-18 12:36:16 +0530

    subscribed to query param.

--- src/app/courses/components/courses/courses.component.ts
+++ src/app/courses/components/courses/courses.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { catchError, Observable, of } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+import { catchError, Observable, of, ReplaySubject, takeUntil } from 'rxjs';
 import { CourseDetails } from 'src/app/booking/interface/booking';
 import { NotificationService } from 'src/app/core/services/notification/notification.service';
 import { CourseRepositoryService } from '../../services/course-repository.service';
@@ -13,9 +14,12 @@ export class CoursesComponent implements OnInit {
 
   courses$!: Observable<CourseDetails[]>
 
+  destroyed$: ReplaySubject<boolean> = new ReplaySubject(1);
+
 
   constructor(private courseRepositoryService: CourseRepositoryService,
-    private notificationService: NotificationService) { }
+    private notificationService: NotificationService,
+    private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.courses$ = this.courseRepositoryService.getCourses().pipe(
@@ -23,6 +27,12 @@ export class CoursesComponent implements OnInit {
         this.notificationService.showWarning('Failed load instituted')
         return of()
       }))
+
+    this.route.queryParams.pipe(
+      takeUntil(this.destroyed$)
+    ).subscribe((response) => {
+      console.log(response);
+    })
   }
 
 }
