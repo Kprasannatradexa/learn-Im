7d527c7e97873e87dd20643c3b75bc8628258ec5
Kprasannatradexa <prasanna@tradexa.co.in>
2023-02-20 17:29:44 +0530

    completedCourses$ is used to store completedCourses.

--- src/app/profile/components/completed-courses/completed-courses.component.ts
+++ src/app/profile/components/completed-courses/completed-courses.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { catchError, Observable, of } from 'rxjs';
+import { UserBookedCourses } from 'src/app/booking/interface/user-booked-courses';
+import { NotificationService } from 'src/app/core/services/notification/notification.service';
+import { UserProfileRepositoryService } from '../../services/user-profile-repository.service';
 
 @Component({
   selector: 'app-completed-courses',
@@ -7,9 +11,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CompletedCoursesComponent implements OnInit {
 
-  constructor() { }
+  completedCourses$!: Observable<UserBookedCourses[]>;
+
+  constructor(private userProfileRepositoryService: UserProfileRepositoryService,
+    private notificationService: NotificationService) { }
 
   ngOnInit(): void {
+    this.completedCourses$ = this.userProfileRepositoryService.getCompletedCourses().pipe(
+      catchError(() => {
+        this.notificationService.showError('Failed to load completed courses.')
+        return of()
+      })
+    )
   }
 
 }
