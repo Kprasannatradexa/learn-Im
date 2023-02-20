960ee35b828c851f741aba4769717de3d37cdebb
Kprasannatradexa <prasanna@tradexa.co.in>
2023-02-20 11:39:33 +0530

    Course detail routing added.

--- src/app/courses/courses-routing.module.ts
+++ src/app/courses/courses-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CourseDetailComponent } from './components/course-detail/course-detail.component';
 import { CoursesComponent } from './components/courses/courses.component';
 
 const routes: Routes = [
-    { path: '', component: CoursesComponent }
+    { path: '', component: CoursesComponent },
+    { path: ':id', component: CourseDetailComponent }
 ];
 
 @NgModule({
