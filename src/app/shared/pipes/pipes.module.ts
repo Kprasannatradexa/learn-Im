2d2ae9e213beb436e987df6c17da3f787ae73d54
Kprasannatradexa <prasanna@tradexa.co.in>
2023-02-20 13:11:16 +0530

    completeAddressConcat exported from pipeModule.

--- src/app/shared/pipes/pipes.module.ts
+++ src/app/shared/pipes/pipes.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MediaBucketPipe } from './media-bucket.pipe';
+import { CompleteAddressConcatPipe } from './complete-address-concat.pipe';
 
 
 
 @NgModule({
   declarations: [
-    MediaBucketPipe
+    MediaBucketPipe,
+    CompleteAddressConcatPipe
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    MediaBucketPipe
+    MediaBucketPipe,
+    CompleteAddressConcatPipe
   ]
 })
 export class PipesModule { }
