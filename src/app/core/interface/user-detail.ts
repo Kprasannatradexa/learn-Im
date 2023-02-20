c84f7afdd8eae4569dedcd602df30dc49e132064
Kprasannatradexa <prasanna@tradexa.co.in>
2023-02-20 16:47:47 +0530

    UserDetail interface created.

---
+++ src/app/core/interface/user-detail.ts
@@ -0,0 +1,17 @@
+export interface UserDetail {
+    id: string;
+    email: string;
+    first_name: string;
+    last_name: string;
+    gender: string,
+    mobile_number: string;
+    profile_photo: null,
+    country: string;
+    state: string;
+    city: string
+    role: string
+    address_line_1: string;
+    address_line_2: string;
+    pincode: string;
+    is_active: boolean;
+}
