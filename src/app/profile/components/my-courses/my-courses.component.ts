df1d71b774de1a12a90d12a55b9822b3516921e8
Kprasannatradexa <prasanna@tradexa.co.in>
2023-02-21 12:08:53 +0530

    Merge branch 'dev' of https://git-codecommit.ap-south-1.amazonaws.com/v1/repos/learn_im_frontend into feature/pipes

--- src/app/profile/components/my-courses/my-courses.component.ts
+++ src/app/profile/components/my-courses/my-courses.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { catchError, Observable, of } from 'rxjs';
 import { UserBookedCourses } from 'src/app/booking/interface/user-booked-courses';
 import { NotificationService } from 'src/app/core/services/notification/notification.service';
 import { UserProfileRepositoryService } from '../../services/user-profile-repository.service';
 
+export enum BookStatus {
+  CONFIRMED = 'Confirmed',
+  PENDING = 'Pending',
+  REJECTED = 'Rejected',
+  CANCELLED = 'Cancelled'
+}
 @Component({
   selector: 'app-my-courses',
   templateUrl: './my-courses.component.html',
   styleUrls: ['./my-courses.component.scss']
 })
 export class MyCoursesComponent implements OnInit {
+
+  @ViewChild('kebabMenu') kebabMenu!: ElementRef;
+
+  BookStatus = BookStatus;
 
   bookedCourses$!: Observable<UserBookedCourses[]>;
 
